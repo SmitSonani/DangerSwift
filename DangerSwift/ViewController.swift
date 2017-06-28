03f577ad1029f7a8a56a64cd37988900bd965a70
STLLPT038_SMIT <smit.sonani@solutelabs.com>
2017-06-28 11:50:25 +0530

    Added default xcode ios app skeleton

---
+++ DangerSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DangerSwift
+//
+//  Created by STLLPT038_SMIT on 28/06/17.
+//  Copyright © 2017 STLLPT038_SMIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
